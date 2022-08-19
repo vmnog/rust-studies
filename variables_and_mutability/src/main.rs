8439d5bac449224a6adb534cbf9aa70da0046a4b
Victor Nogueira <p51901224@brasilprev.com.br>
2022-08-19 16:42:21 -0300

    feat: variables scopes and mutability

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let mut x = 5;
-    println!("x is : {x}");
-    x = 6;
-    println!("x is : {x}");
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of innerscope x is {x}");
+    }
+    
+    println!("The value of x is {x}");
 }
