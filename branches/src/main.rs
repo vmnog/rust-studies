acbd18bded00ba73b31da1bc27888b30fd3d5ee3
Victor Nogueira <p51901224@brasilprev.com.br>
2022-08-21 00:15:37 -0300

    feat: using if / else if statements

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let number = 5;
+    let number = 12;
 
-    if number < 5 {
-        println!("This condition was true");
+    if number % 4 == 0 {
+        println!("{number} is divised by 4");
+    } else if number % 3 == 0 {
+        println!("{number} is divised by 3");
+    } else if number % 2 == 0 {
+        println!("{number} is divised by 2");
     } else {
-        println!("This condition was false");
+        println!("{number} is not divised by 4, 3 or 2");
     }
 }
