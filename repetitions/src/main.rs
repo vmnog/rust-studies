87f84256c91ff7c7ad0111764c78b1a618899b8d
Victor Nogueira <p51901224@brasilprev.com.br>
2022-08-25 10:21:13 -0300

    feat: using while loop

--- repetitions/src/main.rs
+++ repetitions/src/main.rs
@@ -1,27 +1,11 @@
 fn main() {
-    let mut count = 0;
-    
-    'counting_up: loop {
-        println!("count = {count}");
+    let mut number = 3;
 
-        let mut remaining = 10;
+    while number != 0 {
+        println!("{number}!");
 
-        loop {
-            println!("remaining = {remaining}");
-
-            if remaining == 9 {
-                break;
-            }
-            
-            if count == 2 {
-                break 'counting_up;
-            }
-
-            remaining -= 1;
-        }
-
-        count += 1;
+        number -= 1;
     }
 
-    println!("End count = {count}");
+    println!("LIFTOFF!!!!");
 }
